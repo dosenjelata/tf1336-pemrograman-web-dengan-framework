5946925fafc6b0cb52a54024c65be48b079929ec
Dosen Jelata <gunawan@ums.ac.id>
2022-10-11 13:17:14 +1300

    added end of tutorial 4

---
+++ polls/views.py
@@ -0,0 +1,69 @@
+# import from 3rd party module
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+# import from local module
+from .models import Question, Choice
+
+
+
+# Create your views here.
+def index(request):
+    # # tanpa template
+    # return HttpResponse("Hello world, you are at the poll index page!")
+    # dengan template
+    latest_question_list = Question.objects.order_by('-pub_date')[:10]
+    context = {
+        'latest_question_list': latest_question_list,
+        'dataku': "UMS Solo",
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, "polls/detail.html", context)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
